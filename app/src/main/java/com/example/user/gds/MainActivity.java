6be4f97fdc2eed041a1651ee9098d92cea0941b9
Zaznatov E <mngful5@mail.ru>
2016-11-09 22:14:53 +0300

    no message

---
+++ app/src/main/java/com/example/user/gds/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.user.gds;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/user/gds/am2.java
@@ -0,0 +1,17 @@
+package com.example.user.gds;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 06.11.2016.
+ */
+
+public class am2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.am2);
+    }
+}
